83cf34fa21b2a99726c66cf4a42fba192c296731
Jesse <Jesse.m.bevil.mil@gmail.com>
2019-10-21 04:51:19 -0500

    ```CPP  == 10/20/19 == 1.3.00 (Chatwiththisname)  Added comma delimited outputs, IE: 361,987,590 instead of 361987590 for the total dmg output.  created two new functions. ReverseString(PCHAR szLine) and PutCommas(PCHAR szLine) to manipulate the strings.  These should only be used just prior to output to the Listbox. This is now done by default.
     Added new checkbox in the settings menu. Use T B M K Numbers. Which allows for 2.9 mil, or 3.5 k, or 1.5 bil etc.
     outputs instead of 2,905,376 3,527 or 1,587,439,068 outputs to the DPS list.
     Updated the XML file for the above change.
    
     Changed where Anon was happening. It was happening during the entity creation instead of just anonymizing
     the string being written to the listbox for the name. I've changed, so now you can "/caption anon on" mid
     combat and it won't create a new entry in the listbox. ```

--- MQ2Dpsadv.h
+++ MQ2Dpsadv.h
@@ -1,12 +1,12 @@
 // DPS ADV CREATED BY WARNEN 2008-2009
 // MQ2DPSAdv.h
 
-char* OtherHits[] = {" punches "," slashes "," crushes "," pierces "," hits "," kicks "," backstabs "," frenzies on "," bashes ", " strikes "," claws "," slices "," bites "," mauls "," stings ", " shoots ", " smashes ", 0};
-char* YourHits[] = {" hit "," slash "," pierce "," crush "," kick "," punch "," backstab "," frenzy on "," bash ", " strike "," claw "," slice "," bite "," maul "," sting ", " shoot ", " smash ", 0};
+char* OtherHits[] = { " punches "," slashes "," crushes "," pierces "," hits "," kicks "," backstabs "," frenzies on "," bashes ", " strikes "," claws "," slices "," bites "," mauls "," stings ", " shoots ", " smashes ", 0 };
+char* YourHits[] = { " hit "," slash "," pierce "," crush "," kick "," punch "," backstab "," frenzy on "," bash ", " strike "," claw "," slice "," bite "," maul "," sting ", " shoot ", " smash ", 0 };
 enum { CLISTTARGET, CLISTMAXDMG, SINGLE };
 enum { NOTOTAL, TOTALABOVE, TOTALSECOND, TOTALBOTTOM };
 
-static int Coloring []= {
+static int Coloring[] = {
    {0},   //unk
    {13},   //war
    {4},   //clr
@@ -28,45 +28,45 @@ static int Coloring []= {
 
 int ColorTest[17];
 
-PSPAWNINFO   CurTarget;
-time_t      Intervals;
-int         CListType;
-int         MaxDmgLast;
-int         MeColor;
-int         MeTopColor;
-int         NormalColor;
-int         NPCColor;
-int         TotalColor;
-int         EntHover;
-int         EntHighlight;
-int         FightNormal;
-int         FightHover;
-int         FightHighlight;
-int         FightActive;
-int         FightInActive;
-int         FightDead;
-bool      Saved;
-bool      WarnedYHO, WarnedOHO;
-bool      Debug;
-bool      Active;
-bool      Zoning;
-bool      WrongUI;
-
-bool      ShowMeTop;
-bool      ShowMeMin;
-int         ShowMeMinNum;
-bool      UseRaidColors;
-bool      LiveUpdate;
-int         ShowTotal;
-int         FightIA;
-int         FightTO;
-int         EntTO;
+PSPAWNINFO CurTarget;
+time_t Intervals;
+int CListType;
+int MaxDmgLast;
+int MeColor;
+int MeTopColor;
+int NormalColor;
+int NPCColor;
+int TotalColor;
+int EntHover;
+int EntHighlight;
+int FightNormal;
+int FightHover;
+int FightHighlight;
+int FightActive;
+int FightInActive;
+int FightDead;
+bool Saved;
+bool WarnedYHO, WarnedOHO;
+bool Debug;
+bool Active;
+bool Zoning;
+bool WrongUI;
+bool ShowMeTop;
+bool ShowMeMin;
+int ShowMeMinNum;
+bool UseRaidColors;//Use colors from set raid colors for each class display. 
+bool LiveUpdate;//Update Total/DPS/Time/% as it happens instead of a set pulse.
+int ShowTotal;
+int FightIA;//Fight Inactive.
+int FightTO;//Fight Timeout.
+int EntTO;//Entity Timeout.
+bool UseTBMKOutputs = false;//Intended to show 1.4t, 1.5m, 343k etc outputs for total and DPS.
 
 struct EntDamage {
-   int Total;
-   time_t First;
-   time_t Last;
-   int AddTime;
+	unsigned long long Total;
+	time_t First;
+	time_t Last;
+	int AddTime;
 };
 
 PLUGIN_API VOID SetGameState(DWORD GameState);
@@ -76,112 +76,113 @@ PLUGIN_API VOID InitializePlugin(VOID);
 PLUGIN_API VOID ShutdownPlugin(VOID);
 PLUGIN_API DWORD OnIncomingChat(PCHAR Line, DWORD Color);
 PLUGIN_API VOID OnPulse(VOID);
-bool Anonymize(char *name);
-void         DestroyDPSWindow();
-void         CreateDPSWindow();
-void         CreateDPSWindow();
-void         DestroyDPSWindow();
-template <unsigned int _EntSize, unsigned int _MobSize>bool SplitStringOtherHitOther(PCHAR Line, CHAR(&EntName)[_EntSize], CHAR(&MobName)[_MobSize], int *Damage);
-template <unsigned int _EntSize, unsigned int _MobSize>bool SplitStringDOT(PCHAR Line, CHAR(&EntName)[_EntSize], CHAR(&MobName)[_MobSize], int *Damage);
-template <unsigned int _EntSize, unsigned int _MobSize>bool SplitStringNonMelee(PCHAR Line, CHAR(&EntName)[_EntSize], CHAR(&MobName)[_MobSize], int *Damage);
-void         HandleYouHitOther(PCHAR Line);
-void         HandleOtherHitOther(PCHAR Line);
-void         HandleNonMelee(PCHAR Line);
-void         HandleDOT(PCHAR Line);
-void         HandleDeath(PCHAR Line);
-void         TargetSwitch();
-void         CheckActive();
-void         DPSAdvCmd(PSPAWNINFO pChar, PCHAR szLine);
+void CreateDPSWindow();
+void DestroyDPSWindow();
+template <unsigned int _EntSize, unsigned int _MobSize>bool SplitStringOtherHitOther(PCHAR Line, CHAR(&EntName)[_EntSize], CHAR(&MobName)[_MobSize], int* Damage);
+template <unsigned int _EntSize, unsigned int _MobSize>bool SplitStringDOT(PCHAR Line, CHAR(&EntName)[_EntSize], CHAR(&MobName)[_MobSize], int* Damage);
+template <unsigned int _EntSize, unsigned int _MobSize>bool SplitStringNonMelee(PCHAR Line, CHAR(&EntName)[_EntSize], CHAR(&MobName)[_MobSize], int* Damage);
+void HandleYouHitOther(PCHAR Line);
+void HandleOtherHitOther(PCHAR Line);
+void HandleNonMelee(PCHAR Line);
+void HandleDOT(PCHAR Line);
+void HandleDeath(PCHAR Line);
+void TargetSwitch();
+void CheckActive();
+void DPSAdvCmd(PSPAWNINFO pChar, PCHAR szLine);
+void ReverseString(PCHAR szLine);
+void PutCommas(PCHAR szLine);
+void MakeItTBMK(PCHAR szLine);
 #ifdef DPSDEV
 void         DPSTestCmd(PSPAWNINFO pChar, PCHAR szLine);
 #endif
 
 class DPSMob {
 public:
-   class DPSEntry {
-   public:
-      char Name[64];
-      bool DoSort;
-      bool Pets;
-      bool CheckPetName;
-      bool UsingPetName;
-      int SpawnType;
-      int Class;
-      bool Mercenary;
-      EntDamage Damage;
-      DPSMob *Parent;
-      PSPAWNINFO Spawn;
-      DPSEntry *Master;
+	class DPSEntry {
+	public:
+		char Name[64];
+		bool DoSort;
+		bool Pets;
+		bool CheckPetName;
+		bool UsingPetName;
+		int SpawnType;
+		int Class;
+		bool Mercenary;
+		EntDamage Damage;
+		DPSMob* Parent;
+		PSPAWNINFO Spawn;
+		DPSEntry* Master;
 
-      DPSEntry();
-      DPSEntry(char EntName[64], DPSMob *pParent);
-      void      Init();
-      void      AddDamage(int aDamage);
-      int         GetDPS();
-      void      Sort();
-      void      GetSpawn();
-      bool      CheckMaster();
-   };
+		DPSEntry();
+		DPSEntry(char EntName[64], DPSMob* pParent);
+		void Init();
+		void AddDamage(int aDamage);
+		unsigned long long GetDPS();
+		void Sort();
+		void GetSpawn();
+		bool CheckMaster();
+	};
 
-   char Name[64];
-   char Tag[8];
-   int SpawnType;
-   PSPAWNINFO Spawn;
-   bool Active;
-   bool InActive;
-   bool Dead;
-   bool PetName;
-   bool Mercenary;
-   EntDamage Damage;
-   DPSEntry *LastEntry;
-   vector<DPSEntry*> EntList;
+	char Name[64];
+	char Tag[8];
+	int SpawnType;
+	PSPAWNINFO Spawn;
+	bool Active;
+	bool InActive;
+	bool Dead;
+	bool PetName;
+	bool Mercenary;
+	EntDamage Damage;
+	DPSEntry* LastEntry;
+	vector<DPSEntry*> EntList;
 
-            DPSMob();
-			DPSMob::DPSMob(PCHAR MobName, size_t MobLen);
-   void      Init();
-   void      AddDamage(int aDamage);
-   void      GetSpawn();
-   bool      IsPet();
-   DPSEntry   *GetEntry(char EntName[64], bool Create = true);
+	DPSMob();
+	DPSMob::DPSMob(PCHAR MobName, size_t MobLen);
+	void Init();
+	void AddDamage(int aDamage);
+	void GetSpawn();
+	bool IsPet();
+	DPSEntry* GetEntry(char EntName[64], bool Create = true);
 };
+
 vector<DPSMob*> MobList;
-DPSMob   *LastMob;
-DPSMob   *CurTarMob;
-DPSMob   *CurListMob;
-DPSMob   *CurMaxMob;
-template <unsigned int _NameSize>DPSMob *GetMob(CHAR(&Name)[_NameSize], bool Create = true, bool Alive = false);
-bool         MobListMaint(DPSMob *Mob, int ListLoc);
-void         HandleDeath(DPSMob *DeadMob);
-void         ListSwitch(DPSMob *Switcher);
+DPSMob* LastMob;
+DPSMob* CurTarMob;
+DPSMob* CurListMob;
+DPSMob* CurMaxMob;
+template <unsigned int _NameSize>DPSMob* GetMob(CHAR(&Name)[_NameSize], bool Create = true, bool Alive = false);
+bool MobListMaint(DPSMob* Mob, int ListLoc);
+void HandleDeath(DPSMob* DeadMob);
+void ListSwitch(DPSMob* Switcher);
 
 class CDPSAdvWnd : public CCustomWnd {
 public:
-   CTabWnd *Tabs;
-   CListWnd *LTopList;
-   CComboWnd *CMobList;
-   CCheckBoxWnd *CShowMeTop;
-   CCheckBoxWnd *CShowMeMin;
-   CTextEntryWnd *TShowMeMin;
-   CCheckBoxWnd *CUseRaidColors;
-   CCheckBoxWnd *CLiveUpdate;
-   CTextEntryWnd *TFightIA;
-   CTextEntryWnd *TFightTO;
-   CTextEntryWnd *TEntTO;
-   CComboWnd *CShowTotal;
-//   CListWnd *LFightList;
-   bool ReSort;
+	CTabWnd* Tabs;
+	CListWnd* LTopList;
+	CComboWnd* CMobList;
+	CCheckBoxWnd* CShowMeTop;
+	CCheckBoxWnd* CShowMeMin;
+	CTextEntryWnd* TShowMeMin;
+	CCheckBoxWnd* CUseRaidColors;
+	CCheckBoxWnd* CLiveUpdate;
+	CCheckBoxWnd* CUseTBMKOutput;
+	CTextEntryWnd* TFightIA;
+	CTextEntryWnd* TFightTO;
+	CTextEntryWnd* TEntTO;
+	CComboWnd* CShowTotal;
+	bool ReSort;
 
-   CDPSAdvWnd();
-   ~CDPSAdvWnd();
-   void DrawList(bool DoDead = false);
-   void SetTotal(int LineNum, DPSMob *Mob);
-   void DrawCombo();
-   void LoadLoc(char szChar[64] = 0);
-   void LoadSettings();
-   void SaveLoc();
-   void SetLineColors(int LineNum, DPSMob::DPSEntry *Ent, bool Total = false, bool MeTop = false);
-   int WndNotification(CXWnd *pWnd, unsigned int Message, void *unknown);
-   void SaveSetting(PCHAR Key, PCHAR Value, ...);
+	CDPSAdvWnd();
+	~CDPSAdvWnd();
+	void DrawList(bool DoDead = false);
+	void SetTotal(int LineNum, DPSMob* Mob);
+	void DrawCombo();
+	void LoadLoc(char szChar[64] = 0);
+	void LoadSettings();
+	void SaveLoc();
+	void SetLineColors(int LineNum, DPSMob::DPSEntry* Ent, bool Total = false, bool MeTop = false);
+	int WndNotification(CXWnd* pWnd, unsigned int Message, void* unknown);
+	void SaveSetting(PCHAR Key, PCHAR Value, ...);
 
 };
-CDPSAdvWnd *DPSWnd = 0;
+CDPSAdvWnd* DPSWnd = 0;
